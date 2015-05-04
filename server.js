7e83b17a0f7f94a6d80e70aa18a57ec28bcf92ce
sumitjangid <sumitjan07@gmail.com>
2015-05-03 20:20:03 -0700

    mongoose implementation

--- server.js
+++ server.js
@@ -6,14 +6,42 @@ var express = require('express')
   , server = http.createServer(app)
   ,Twit = require('twit')
   ,sentiment = require('sentiment')
-  , io = require('socket.io').listen(server);
+  , io = require('socket.io').listen(server)
+  , mongoose = require("mongoose");
 
+mongoose.connect('mongodb://localhost/');
+
+// This is our mongoose model for todos
+var db = mongoose.connection;
 server.listen(8080);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
 res.sendFile(__dirname + '/index.html');
 });
+
+app.get("/todos.json", function (req, res) {
+    ToDo.find({}, function (err, toDos) {
+  res.json(toDos);
+    });
+});
+
+//mongoosedb
+db.on('error', function (err) {
+console.log('connection error', err);
+});
+db.once('open', function () {
+console.log('connected.');
+});
+
+var Schema = mongoose.Schema;
+var tweetSchema = new Schema({
+id    : Number, 
+score : String,
+tweet : String
+});
+
+var tweetScore = mongoose.model('tweetScore', tweetSchema);
 
 //sentiment test
 //var result_s = sentiment('What an asshole that guy is');
@@ -32,14 +60,27 @@ var sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ];
 io.sockets.on('connection', function (socket) {
   console.log('Connected');
 
-
+ var tweet_id = 0;
  var stream = T.stream('statuses/filter', { track: watchList })
  //var stream = T.stream('statuses/filter', { locations: sanFrancisco })
 
   stream.on('tweet', function (tweet) {
+
     // console.log(tweet);
     var result_s = sentiment(tweet.text);
     //console.log(result_s);
+    tweet_id++;
+    var newTweet = new tweetScore({
+      id    : tweet_id,
+      score : result_s.score,
+      tweet : tweet.text
+    });
+
+    newTweet.save(function (err, data) {
+    if (err) console.log(err);
+    else console.log('Saved ', data );
+    });
+
     io.sockets.emit('stream',tweet);
     io.sockets.emit('sentiment',result_s);
   });
